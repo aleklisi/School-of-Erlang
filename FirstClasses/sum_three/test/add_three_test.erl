15d5136b90cdbc86626dc7d7fb90108dd120cba0
Aleksander Lisiecki <sebaczechu123@gmail.com>
2019-11-20 07:36:52 +0100

    Add property based testing example

--- FirstClasses/sum_three/test/add_three_test.erl
+++ FirstClasses/sum_three/test/add_three_test.erl
@@ -1,5 +1,6 @@
 -module(add_three_test).
 
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 add_three_test() ->
@@ -7,3 +8,10 @@ add_three_test() ->
     ?assertEqual(1, sum_three:add_three(1, -1, 1)),
     ?assertEqual(4, sum_three:add_three(2, 1, 1)),
     ?assertEqual(-3, sum_three:add_three(-1, -1, -1)).
+
+add_three_proper_test() ->
+    Property = ?FORALL(
+        {A, B, C},
+        {integer(), integer(), integer()},
+        lists:sum([A, B, C]) =:= sum_three:add_three(A, B, C)),
+    ?assert(proper:quickcheck(Property, 100000)).
